ca942000ceae40af6210c5cc9d271971f430485d
cameron <cameron.w.cooper@gmai.com>
2020-02-11 23:45:04 -0700

    added docker, docker-compose, circleci, portainer, redis

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DevOps
 A project to play with devops tooling
+
+## Dependencies
+* Docker
+* Docker-Compose
+* Python 3.7
+
+### Current Stack
+* Flask (Frontend)
+    * [localhost:5000](localhost:5000)
+* Redis (Caching)
+* Docker (Container)
+* Docker-Compose (Container Orchestration)
+* Portainer (Container Dashboard)
+    * [localhost:9000](localhost:9000)
+* CircleCI
--- web/app.py
+++ web/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from redis import Redis
 
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    redis.incr('hits')
+    return render_template('index.html', hits=redis.get('hits'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
